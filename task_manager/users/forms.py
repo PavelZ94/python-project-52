cc8f24bc4942e04fdd9f485e9da969e5c717d261
Павел <zhilinsky.pa@yandex.ru>
2024-06-05 23:43:58 +0300

    added templates

---
+++ task_manager/users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('first_name', 'last_name', 'username')
