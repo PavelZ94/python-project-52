ffdc91cd55dd9ce272470b323a2f20d36fd5b447
Павел <zhilinsky.pa@yandex.ru>
2024-06-13 23:20:52 +0300

    tests

--- Makefile
+++ Makefile
@@ -11,6 +11,8 @@ PORT ?= 8000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) task_manager.wsgi
 
-
 shell:
 	$ ./manage.py shell --ipython
+
+test:
+	python manage.py test task_manager
---
+++ task_manager/tests/test_users.py
@@ -0,0 +1,106 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from task_manager.users.models import User
+
+class CRUDTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.test_user = User.objects.create(username='Sum41',
+                                             password='R0ckStar')
+
+
+    def test_user_create(self):
+        username = 'Test'
+        first_name = 'testname'
+        last_name = 'surname'
+        password = 'test1234Q'
+
+        response = self.client.post(
+            reverse('user_create'),
+            data={'username': username,
+                  'first_name': first_name,
+                  'last_name': last_name,
+                  'password1': password,
+                  'password2': password}
+        )
+
+        self.assertRedirects(response, reverse('login'), 302)
+
+        new_user = User.objects.get(username=username)
+
+        self.assertEqual(new_user.username, username)
+        self.assertEqual(new_user.first_name, first_name)
+        self.assertEqual(new_user.last_name, last_name)
+
+
+    def test_user_update(self):
+        username = 'Sum41'
+        first_name = 'Derick'
+        last_name = 'Wilby'
+        password = 'R0ckStar'
+
+        self.client.post(
+            reverse('user_create'),
+            data={'username': username,
+                  'first_name': first_name,
+                  'last_name': last_name,
+                  'password1': password,
+                  'password2': password}
+        )
+
+        self.client.post(
+            reverse('login'),
+            data={'username': username,
+                  'password': password}
+        )
+
+        user = User.objects.get(username=username)
+
+        changed_first_name = 'Pavel'
+        response = self.client.post(
+            reverse('user_update', kwargs={'pk': user.pk}),
+            data={#'username': username,
+                  'first_name': changed_first_name,
+                  'last_name': last_name,
+                  'password1': password,
+                  'password2': password}
+        )
+
+        self.assertRedirects(response, reverse('users', 302))
+
+        user.refresh_from_db()
+        self.assertEqual(user.first_name, changed_first_name)
+
+    def test_user_delete(self):
+        username = 'Nirvana'
+        first_name = 'Kurt'
+        last_name = 'Cobain'
+        password = 'Sho0ter94'
+
+        self.client.post(
+            reverse('user_create'),
+            data={'username': username,
+                  'first_name': first_name,
+                  'last_name': last_name,
+                  'password1': password,
+                  'password2': password}
+        )
+
+        self.client.post(
+            reverse('login'),
+            data={'username': username,
+                  'password': password}
+        )
+
+        user = User.objects.get(username=username)
+
+        response = self.client.post(
+            reverse('user_delete', kwargs={'pk': user.pk})
+        )
+
+        self.assertRedirects(response, reverse('users'), 302)
+
+        deleted_user = User.objects.filter(id=user.pk)
+        self.assertNotIn(user.pk, deleted_user)
