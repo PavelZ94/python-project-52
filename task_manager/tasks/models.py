29f2e7345947112ecd09642e27cb55eb32903d7c
Павел <zhilinsky.pa@yandex.ru>
2024-06-29 00:32:12 +0300

    try

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -11,8 +11,8 @@ class Task(models.Model):
     name = models.CharField(max_length=100,
                             unique=True,)
 
-    #description = models.TextField(blank=True,
-    #                               null=True,)
+    description = models.TextField(blank=True,
+                                   null=True,)
 
     author = models.ForeignKey(User,
                                null=True,
