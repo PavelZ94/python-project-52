170eca0f9fba15f0ae455234339359fe6979162a
Павел <zhilinsky.pa@yandex.ru>
2024-06-29 18:21:55 +0300

    finish updates

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,17 +1,8 @@
-from django import forms
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.translation import gettext as _
 
 
 class CreationForm(UserCreationForm):
-
-    #first_name = forms.CharField(max_length=150,
-    #                             required=True,
-    #                             label=_('First name'))
-    #last_name = forms.CharField(max_length=150,
-    #                            required=True,
-    #                            label=_('Last name'))
 
     class Meta(UserCreationForm.Meta):
         model = User
